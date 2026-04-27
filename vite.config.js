af414d46db8582f8610afc577109659f52ae27a7
Ivan <itsvanka@gmail.com>
2026-04-27 00:45:42 -0400

    config revert

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "https://ivan-chernyshov.com/",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
