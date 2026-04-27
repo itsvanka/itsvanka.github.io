e0d3379bde026f7a97a46e19bac942eebb0d0c51
Ivan <itsvanka@gmail.com>
2026-04-27 01:04:28 -0400

    more configs

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName: (name) => {
+          // Sanitizes file names generated during the build process:
+          // - Replaces spaces with dashes ('-').
+          // - Removes invalid characters that are not alphanumeric, underscores (_), periods (.), or dashes (-).
+          return name
+            .replace(/\s+/g, '-') // Replaces spaces with dashes.
+            .replace(/[^a-zA-Z0-9_.-]/g, ''); // Removes all invalid characters.
+        },
+      },
+    },
+  },
 })
